7aa18d21ec0db684b125586961856436c347feb2
mateuszgadomski <mateusz.gadomski.dev@gmail.com>
2023-01-17 21:05:30 +0100

    UI update

--- Assets/Scripts/HudUI.cs
+++ Assets/Scripts/HudUI.cs
@@ -1,4 +1,5 @@
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,59 +7,52 @@ public class HudUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI healthText;
     [SerializeField] private TextMeshProUGUI coinsText;
-    [SerializeField] private TextMeshProUGUI shopText;
-    [SerializeField] private TextMeshProUGUI pointsText;
-    [SerializeField] private GameObject statsPanel;
-    [SerializeField] private PlayerStats playerStats;
 
-    public Image[] backgroundButtons;
+    private Image[] backgroundButtons;
+    private TextMeshProUGUI[] texts;
 
     private readonly string healthTextTitle = "HEALTH";
     private readonly string coinsTextTitle = "COINS";
 
-    private void Awake()
-    {
-        statsPanel.GetComponentsInChildren<Image>();
-    }
-
     private void Start()
     {
+        texts = GetComponentsInChildren<TextMeshProUGUI>();
+        backgroundButtons = GetComponentsInChildren<Image>();
+
         EventManager.PlayerEvents.OnPlayerHealthChangeCallback += OnHealthValueChange;
         EventManager.PlayerEvents.OnPlayerCoinsValueChangeCallback += OnCoinsValueChange;
         EventManager.LevelEvents.OnLevelChangeLightColorsCallback += ChangeHudTextColors;
-        EventManager.LevelEvents.OnlevelChangeDarkColorsCallback += ChangeColorBackgroundButtons;
+        EventManager.LevelEvents.OnlevelChangeDarkColorsCallback += ChangeHudBackgroundColor;
     }
     private void OnDestroy()
     {
         EventManager.PlayerEvents.OnPlayerHealthChangeCallback -= OnHealthValueChange;
         EventManager.PlayerEvents.OnPlayerCoinsValueChangeCallback -= OnCoinsValueChange;
         EventManager.LevelEvents.OnLevelChangeLightColorsCallback -= ChangeHudTextColors;
-        EventManager.LevelEvents.OnlevelChangeDarkColorsCallback -= ChangeColorBackgroundButtons;
+        EventManager.LevelEvents.OnlevelChangeDarkColorsCallback -= ChangeHudBackgroundColor;
     }
 
     public void OnHealthValueChange(float playerHealth) => healthText.text = $"{healthTextTitle} {playerHealth}";
 
     public void OnCoinsValueChange(float playerCoins) => coinsText.text = $"{coinsTextTitle} {playerCoins}";
 
-    private void ChangeTextColor(TextMeshProUGUI textName, Color32 color) =>  textName.color = color;
-
     public void ChangeHudTextColors(Color32 color)
     {
-        ChangeTextColor(healthText, color);
-        ChangeTextColor(coinsText, color);
-        ChangeTextColor(shopText, color);
-        ChangeTextColor(pointsText, color);
+        foreach (var text in texts)
+        {
+            text.color = color;
+        }
     }
 
-    public void ChangeColorBackgroundButtons(Color32 color)
+    public void ChangeHudBackgroundColor(Color32 color)
     {
-        Debug.Log(color);
-        foreach (Image backgroundButton in backgroundButtons)
+        foreach (var backgroundButton in backgroundButtons)
         {
-            Debug.Log(backgroundButton.gameObject);
             backgroundButton.color = color;
         }
     }
+
+  
 
 
 }
